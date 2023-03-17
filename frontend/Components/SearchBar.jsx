c7dc9abe15f68995f591ce15c4b77950d4ee8ae5
deepkore <deepkore185@gmail.com>
2023-03-17 23:39:23 +0530

    Fixed bugs related to merging

--- frontend/Components/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-function SearchBar() {
-    return (
-      <div>
-        <h2>Search Topics</h2>
-        <input type="text" placeholder="Search Anything" />
-      </div>
-    );
-  }
-  
---
+++ frontend/src/page/UI.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import TopicCard from "../components/TopicCard";
+import SearchBar from "../components/SearchBar";
+
+export default function UI() {
+    return (
+        <div>
+            <TopicCard title="Hello" desc=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur nesciunt, quos dicta suscipit totam sit ea neque? Distinctio, totam architecto." postCount={10} onlineCount={5} />
+            <SearchBar />
+        </div>
+    )
+}
