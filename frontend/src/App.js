3c1768817911b99d9b528c62408ba748327133de
akash-k-07 <akashkari1999@gmail.com>
2023-03-17 23:02:50 +0530

    Merged Football and Interview to TopicCard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/football">Football</Link>
+            </li>
+            <li>
+              <Link to="/interview-prep">Interview Prep</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/search">
+            <SearchBar />
+          </Route>
+          <Route path="/football">
+            <Football />
+          </Route>
+          <Route path="/interview-prep">
+            <InterviewPrep />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
+
 
 export default App;
