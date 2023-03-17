bb4ce1eb63e326cef8881022a79bd8e968103948
Ajoy10 <achu.ajoy10@gmail.com>
2023-03-17 19:59:09 +0530

    Setup frontend and server structure

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Setting up CORS
+
+const whitelist = process.env.WHITELISTED_DOMAINS
+  ? process.env.WHITELISTED_DOMAINS.split(",")
+  : [];
+
+const corsOption = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+
+  credential: true,
+};
+
+app.use(cors(corsOption));
+
+//#region Server listening Setup
+
+// Setting up server
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server running successfully!");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Server running at http://localhost:" + port);
+  }
+});
+// #endregion
