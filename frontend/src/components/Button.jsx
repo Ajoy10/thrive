d41064db93c88071dbafde157923722b30ded18b
deepkore <deepkore185@gmail.com>
2023-03-18 13:07:04 +0530

    thread card css creation
    topic page css

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,10 +1,11 @@
+import "../ComponentsCss/button.css"
 import React from "react";
 import { Icon, InlineIcon } from '@iconify/react';
 const Button = ({ label, onClick }) => {
   return (
     <button onClick={onClick}>
       {label}
-    <InlineIcon icon={"carbon:user-avatar"} />
+      <InlineIcon icon={"carbon:user-avatar"} />
     </button>
   );
 };
--- frontend/src/page/UI.jsx
+++ frontend/src/page/UI.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import TopicCard from "../components/TopicCard";
 import SearchBar from "../components/SearchBar";
 import ThreadCard from "../components/ThreadCard";
+import Button from "../components/Button";
 
 export default function UI() {
   return (
@@ -13,13 +14,18 @@ export default function UI() {
         postCount={10}
         onlineCount={5}
       />
+      <br></br>
       <SearchBar />
+      <br></br>
       <ThreadCard
         title="what is the best"
         content="  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur nesciunt, quos dicta suscipit totam sit ea neque? Distinctio, totam architecto."
-        date={20}
+        date={`Today`}
         commentCount={20}
         username="abcd"
+      />
+      <Button
+        label="create a topic"
       />
     </div>
   );
